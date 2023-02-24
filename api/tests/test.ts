6de853e2d8b354e571e44522a891d97060cb8df9
blueyellowpink <andrew@trasua.dev>
2023-02-25 00:49:20 +0700

    update

--- api/tests/test.ts
+++ api/tests/test.ts
@@ -1,4 +1,4 @@
-import Websocket from 'ws'
+/* import Websocket from 'ws'
 
 const ws = new Websocket(
     'ws://localhost:3000/streams?symbol=BTC/USDT&timeWindow=12'
@@ -10,4 +10,49 @@ ws.on('open', () => {
 
 ws.on('message', (data: string) => {
     console.log('received ', JSON.parse(data))
+}) */
+
+const forEach = (items, callback) => {
+	for (let index = 0; index < items.length; index++) {
+		callback(items[index])
+	}
+}
+
+test('test', async () => {
+	const mockCallback = jest.fn(x => 1 + x)
+	console.log(mockCallback.mock.calls)
+
+	forEach([0, 1], mockCallback)
+
+	console.log(mockCallback.mock.calls)
+
+	const mockFn = jest.fn()
+	mockFn.mockReturnValueOnce(10).mockReturnValueOnce(20).mockReturnValue(true)
+	console.log(mockFn(1), mockFn(), mockFn())
+
+	expect(mockFn).toHaveBeenCalled()
+	expect(mockFn).toHaveBeenCalledWith(1)
+})
+
+const funcs = {
+	a: () => 'a'
+}
+
+class API {
+	static hit() {
+		return funcs.a()
+	}
+}
+
+test('test #2', async () => {
+	console.log(API.hit())
+	// jest.spyOn(funcs, 'a').mockImplementationOnce(() => 'b')
+	jest.spyOn(funcs, 'a').mockReturnValueOnce('b')
+
+	const mockData = {
+		data: 'mock',
+	}
+
+	expect(API.hit()).toBe('b')
+	expect(API.hit()).toBe('a')
 })
